7f01aa9e773994d6f300830b9b8b7459af794af1
Wladimir David Zakrevskyy <wd.zakrevskyy@icloud.com>
2017-04-21 00:14:31 +0200

    - reading files is ready

--- src/de/wdz/xml2latex/Main.java
+++ src/de/wdz/xml2latex/Main.java
@@ -1,5 +1,19 @@
 package de.wdz.xml2latex;
 
+import de.wdz.xml2latex.input.XMLReader;
+
 public class Main {
 
+	private static String ru = "resources/SF_2009-01-20_RUS_RUSSUB_(RUSSIAN VERSION).xml";
+	private static String ua = "resources/SF_2009-01-23_RUS_UKRUB_(UKRAINIAN VERSION).xml";
+	private static String de = "resources/SF_2014-09-30_GER_NEö_(NEUE EVANGELISTISCHE öBERSETZUNG (NEö)).xml";
+	private static String en = "resources/SF_2015-08-14_ENG_BWE96_(BIBLE IN WORLDWIDE ENGLISH).xml";
+	
+	private static String enK = "resources/SF_2009-01-20_ENG_KJV_(KJV+).xml";
+	
+	public static void main(String[] args) {
+		XMLReader reader = new XMLReader(en);
+		String test = reader.getBibleTextAsString();
+		System.out.println(test);
+	}
 }
--- src/de/wdz/xml2latex/input/XMLReader.java
+++ src/de/wdz/xml2latex/input/XMLReader.java
@@ -1,5 +1,28 @@
 package de.wdz.xml2latex.input;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class XMLReader {
 
+	private String bibleText;
+
+	public XMLReader(String pathToFile) {
+		bibleText = readFile(pathToFile);
+	}
+
+	public String getBibleTextAsString() {
+		return bibleText;
+	}
+
+	private String readFile(String pathToFile) {
+		try {
+			byte[] encoded = Files.readAllBytes(Paths.get(pathToFile));
+			return new String(encoded);
+		} catch (Throwable e) {
+			System.err.println("error on read file " + e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
